21ae5a29d8b31d2ef03302c91abbe09f180bbafb
G. Kay Lee <balancetraveller+github@gmail.com>
2015-03-19 14:18:31 +0800

    [breaking] Adopt new API

--- expose-via-accessor.babel.js
+++ expose-via-accessor.babel.js
@@ -5,17 +5,19 @@ export default {
     _privateObject = privateObject;
   },
 
-  via(publicObject, transform = x => x) {
-    return Object.defineProperties(publicObject, Object.keys(_privateObject)
-                                                       .reduce((m, n) => {
-                                                         m[n] = {
-                                                           configurable: true,
-                                                           enumerable: true,
-                                                           get: () => _privateObject[n],
-                                                           set: x => _privateObject[n] = transform(x)
-                                                         };
+  via(publicObject) {
+    return (transform = x => x) => {
+      return Object.defineProperties(publicObject, Object.keys(_privateObject)
+                                                         .reduce((m, n) => {
+                                                           m[n] = {
+                                                             configurable: true,
+                                                             enumerable: true,
+                                                             get: () => _privateObject[n],
+                                                             set: x => _privateObject[n] = transform(x)
+                                                           };
 
-                                                         return m;
-                                                       }, {}));
+                                                           return m;
+                                                         }, {}));
+    };
   }
 };
